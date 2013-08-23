051880872a76f1b86c4b232da76b5f58eabdee56
Alperen <alperen.talaslioglu@gmail.com>
2013-08-23 09:43:38 +0300

    Main Class Created

---
+++ creW/src/Scheduler.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+
+
+//SplitPaneDemo itself is not a visible component.
+public class Scheduler {
+
+	public static void main(String[] args) {
+
+		JFrame mainFrame = new JFrame("Cabin Crew Planning");
+		mainFrame.setSize(500,400);
+		mainFrame.setVisible(true);
+	}
+
+}
